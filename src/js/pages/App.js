338f963e255c2289358b536d9d1dcd129ea7e3a1
Brady02 <72949888+Brady02@users.noreply.github.com>
2024-04-12 18:44:30 -0300

    added home pages, ready for animation

---
+++ src/js/pages/App.js
@@ -0,0 +1,26 @@
+import '../../scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+        <div className="name-page">
+          <div className='name-cont'>
+            <p className='hi'>Hi i'm</p>
+            <h1 className="name">BRADY</h1>
+            <h1 className="name">WARREN</h1>
+          </div>
+        </div>
+
+        <div className="title-page">
+          <div className='title-cont'>
+            <p className='hi2'>I'm a</p>
+            <h1 className="title">SOFTWARE</h1>
+            <h1 className="title">DEVELOPER</h1>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
